0ec89500bf9473a59add48b75eb885edfa693eb3
billy <luisenriquec@proton.me>
2024-03-06 20:29:10 -0600

    ejercicio done

--- C6 Bucles/ejercicios/04.js
+++ C6 Bucles/ejercicios/04.js
@@ -2,7 +2,11 @@ function mayoriaDeEdad(edad) {
   // Determinar si la persona puede ingresar al evento según su edad.
   // Si tiene 18 años o más, retorna el string: "Allowed".
   // Caso contrario: "Not allowed".
-  // Tu código:
+  if(edad >= 18){
+    return 'allowed'
+  } else {
+    return 'not allowed'
+  }
 }
 
 module.exports = mayoriaDeEdad;
